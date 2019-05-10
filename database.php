3d0920db5235c34b201e9aea7b0a2b2d1eb07ae0
edwin garcia <edwingarbol95@hotmail.com>
2019-05-10 11:18:40 -0500

    actualizacion repo

--- database.php
+++ database.php
@@ -1,16 +1,17 @@
 <?php
-	//credenciales de conexiòn a BD
-	//acceder a 127.0.0.1/market/database.php .. para efecto de prueba
-	$username = "root";
-	$servername = "localhost"; //ò  127.0.01
-	$password = "";
-	$dbname = "market";
-	
-	$conn = new mysqli($servername,$username,$password,$dbname);
-	
-	if ($conn->connect_error){ //si la conección tiene un error..
-			die("Error: ". $conn->connect_error);
-	}else {
-			//die ("Conexiòn Existosa a Market");
-	}	
+    $servername="localhost";
+    $username="root";
+    $pass="";
+    $DBname="market";
+
+    //create connection to database
+
+    $conn = new mysqli($servername,$username,$pass,$DBname);
+    //cheking
+    if($conn->connect_error){
+        die("Connection failed".$conn->connect_error);
+    }else{
+        //echo "i am connectes to tienda";
+    }
+
 ?>
